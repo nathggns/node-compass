b59f0003dc89ac5c7908bb0a29d7ae0dc87b304e
Nathaniel Higgins <nat@nath.is>
2012-09-09 15:40:43 +0100

    Fix indentation

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 var spawn = require('child_process').spawn,
-	path = require('path'),
-	defaults = {
-		'mode': 'compress',
-		'comments': false,
-		'relative': true,
-		'css': 'stylesheets',
-		'sass': 'stylesheets',
-		'project': path.join(__dirname, 'public')
-	};
+    path = require('path'),
+    defaults = {
+      'mode': 'compress',
+      'comments': false,
+      'relative': true,
+      'css': 'stylesheets',
+      'sass': 'stylesheets',
+      'project': path.join(__dirname, 'public')
+    };
 
 module.exports = exports = function(opts) {
-	opts = opts || {};
-	for (var key in defaults) {
-		if (opts[key] === undefined) {
-			opts[key] = defaults[key];
-		}
-	}
+  opts = opts || {};
+  for (var key in defaults) {
+    if (opts[key] === undefined) {
+      opts[key] = defaults[key];
+    }
+  }
 
-	return function(req, res, next) {
-		
-		var compass = spawn(
-			'compass',
-			[
-				'compile',
-				opts.comments ? '' : '--no-line-comments',
-				opts.relative ? '--relative-assets' : '',
-				'-s', opts.mode,
-				'--css-dir', opts.css,
-				'--sass-dir', opts.sass
-			],
-			{
-				cwd: opts.project
-			}
-		);
+  return function(req, res, next) {
+    var compass = spawn(
+      'compass',
+      [
+        'compile',
+        opts.comments ? '' : '--no-line-comments',
+        opts.relative ? '--relative-assets' : '',
+        '-s', opts.mode,
+        '--css-dir', opts.css,
+        '--sass-dir', opts.sass
+      ],
+      {
+        cwd: opts.project
+      }
+    );
 
-		compass.on('exit', function(code) {
-			return next();
-		});
-	};
+    compass.on('exit', function(code) {
+      return next();
+    });
+  };
 };
